6ea6a0156a437fbfaed606e2c346549f597c9d24
Porrith <porrith.suong@gmail.com>
2020-11-11 10:04:29 -0500

    Fixed colliders to stop the player from levitating when moving.

--- Assets/Scripts/AngryBots.Core/Systems/Player/PlayerMovementSystem.cs
+++ Assets/Scripts/AngryBots.Core/Systems/Player/PlayerMovementSystem.cs
@@ -1,4 +1,5 @@
 using Unity.Collections;
+using Unity.Collections.LowLevel.Unsafe;
 using Unity.Entities;
 using Unity.Mathematics;
 using Unity.Physics;
@@ -8,8 +9,8 @@ using UnityEngine;
 
 namespace AngryBots2.Core.Player.Systems {
 
-    [UpdateInGroup(typeof(FixedStepSimulationSystemGroup))]
-    [UpdateBefore(typeof(BuildPhysicsWorld))]
+    // [UpdateInGroup(typeof(FixedStepSimulationSystemGroup))]
+    // [UpdateBefore(typeof(BuildPhysicsWorld))]
     public unsafe class PlayerMovementSystem : SystemBase {
 
         struct CameraInfo {
@@ -19,35 +20,43 @@ namespace AngryBots2.Core.Player.Systems {
         }
 
         private BuildPhysicsWorld buildPhysicsWorld;
-        static readonly uint GroundMask = 1u << 0;
+        private CameraInfo* camInfo;
 
         protected override void OnCreate() {
             buildPhysicsWorld = World.GetExistingSystem<BuildPhysicsWorld>();
+            camInfo = (CameraInfo*)UnsafeUtility.Malloc(
+                UnsafeUtility.SizeOf<CameraInfo>(), 
+                UnsafeUtility.AlignOf<CameraInfo>(), 
+                Allocator.Persistent);
+        }
+
+        protected override void OnDestroy() {
+            UnsafeUtility.Free(camInfo, Allocator.Persistent);
         }
 
         protected unsafe override void OnUpdate() {
 
             float deltaTime = Time.DeltaTime;
-            CameraInfo* camInfo = stackalloc CameraInfo[1];
+            CameraInfo* local = camInfo;
 
             Entities.WithAll<Camera>().ForEach((in LocalToWorld c0) => {
-                camInfo->Position = c0.Position;
-                camInfo->Forward  = c0.Forward;
-                camInfo->Right    = c0.Right;
-            }).WithNativeDisableUnsafePtrRestriction(camInfo).Run();
+                local->Position = c0.Position;
+                local->Forward  = c0.Forward;
+                local->Right    = c0.Right;
+            }).WithNativeDisableUnsafePtrRestriction(local).Run();
 
             ComponentDataFromEntity<PhysicsCollider> colliders = GetComponentDataFromEntity<PhysicsCollider>(true);
             CollisionWorld collisionWorld                      = buildPhysicsWorld.PhysicsWorld.CollisionWorld;
             NativeArray<RigidBody> bodies                      = buildPhysicsWorld.PhysicsWorld.Bodies;
 
-            Entities.ForEach((ref Translation c0, in InputContainer c1, in Speed c2) => {
+            Entities.ForEach((ref PhysicsVelocity c0, in InputContainer c1, in Speed c2, in Translation c3) => {
 
-                float3 forward = camInfo->Forward;
-                float3 right   = camInfo->Right;
+                float3 forward = local->Forward;
+                float3 right   = local->Right;
                 forward.y = right.y = 0;
 
                 float3 dir = math.normalizesafe(forward * c1.Axis.y + right * c1.Axis.x);
-                float3 start = c0.Value + new float3(0f, 10f, 0f);
+                float3 start = c3.Value + new float3(0f, 10f, 0f);
                 float3 next  = start + dir;
                 next.y = -10f;
 
@@ -66,11 +75,15 @@ namespace AngryBots2.Core.Player.Systems {
                     uint belongsTo = collider.ColliderPtr->Filter.BelongsTo;
 
                     if ((belongsTo & Filters.GroundMask) > 0) {
-                        float3 movement = math.normalizesafe(hitInfo.Position - c0.Value);
-                        c0.Value += movement * deltaTime * c2.Value;
+                        float3 movement = math.normalizesafe(hitInfo.Position - c3.Value);
+                        c0.Linear = movement * deltaTime * c2.Value;
+                    } else {
+                        c0.Linear = float3.zero;
                     }
+                } else {
+                    c0.Linear = float3.zero;
                 }
-            }).WithNativeDisableUnsafePtrRestriction(camInfo).Run();
+            }).WithNativeDisableUnsafePtrRestriction(local).Run();
         }
     }
 }
